1b4570e872f30e4f2b56e79f523911b6abe19fb0
arunprasadh7 <m.arunprasadh96@gmail.com>
2023-12-01 20:18:05 +0530

    prompt function

---
+++ script.js
@@ -0,0 +1,12 @@
+//user input
+// 1 - prompt
+
+// let name = prompt('What\'s your name?')
+// console.log(`Hello ${name}`)
+
+// let tickets = prompt('How many tickets do you want?')
+// console.log(`You have to pay ${tickets * 25} rupees.`)
+
+let a  = prompt('Enter number : ')
+a = Number(a)
+console.log(`Result is ${a + 10}.`)
