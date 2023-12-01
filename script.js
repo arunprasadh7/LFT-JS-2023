2daf56d05cc4a6720e02fc2c13b3b1d5060b0adf
arunprasadh7 <m.arunprasadh96@gmail.com>
2023-12-01 21:23:23 +0530

    DOM

--- script.js
+++ script.js
@@ -7,6 +7,11 @@
 // let tickets = prompt('How many tickets do you want?')
 // console.log(`You have to pay ${tickets * 25} rupees.`)
 
-let a  = prompt('Enter number : ')
-a = Number(a)
-console.log(`Result is ${a + 10}.`)
+// let a  = prompt('Enter number : ')
+// a = Number(a)
+// console.log(`Result is ${a + 10}.`)
+
+
+// DOCUMENT OBJECT MODEL (DOM)
+
+document.getElementById('message').textContent = 'Hello'
